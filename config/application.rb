dbd85f84dc61b86d1ebf85831a2f54c25fbb93f9
okouam <olivier.kouame@gmail.com>
2011-08-24 11:49:09 +0100

    Added fingerprintless assets

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,6 @@ module Geodemo
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     config.assets.enabled = true
+    config.assets.fingerprinting.enabled = false
   end
 end
